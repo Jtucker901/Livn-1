72e87c065d47c8ecf280245ca0eca7e775a30584
Brylan Donaldson <brylan@brylans-air.devices.brown.edu>
2017-02-04 18:35:16 -0600

    added labels for expense view

--- Livn/expenseVC.swift
+++ Livn/expenseVC.swift
@@ -10,4 +10,16 @@ import Foundation
 import UIKit
 
 class expenseViewController: UIViewController {
+    
+    @IBOutlet var incomeField: UITextField!
+    @IBOutlet var rentUtilField: UITextField!
+    @IBOutlet var transportationField: UITextField!
+    @IBOutlet var groceriesField: UITextField!
+    @IBOutlet var personalCareField: UITextField!
+    @IBOutlet var eatingOutField: UITextField!
+    @IBOutlet var entertainmentField: UITextField!
+    @IBOutlet var misField: UITextField!
+    @IBAction func budgetButton(_ sender: Any) {
+    }
+    
 }
